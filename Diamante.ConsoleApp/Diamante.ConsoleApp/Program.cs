022fe213cb72b63a3237e2bb8ff0631395bea338
Kaue Ramos <ramoskauegriss@gmail.com>
2023-03-16 21:54:31 -0300

    Robô e diamante

---
+++ Diamante.ConsoleApp/Diamante.ConsoleApp/Program.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Diamante.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+                       inicio:
+			try
+			{
+                Console.Title = "Diamante";
+
+                Console.WriteLine("Escolha como desenhar o diamante: ");
+                Console.WriteLine("1 - Alfabeto");
+                Console.WriteLine("2 - X");
+                int Escolha = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine();
+
+                if (Escolha == 1)
+                {
+                    #region Diamante Alfabeto
+                    Console.Clear();
+
+                    Console.WriteLine("Informe uma letra: ");
+                    char Letra = char.Parse(Console.ReadLine());
+
+                    int Tamanho = (Letra - 'A') * 2 + 1;
+
+                    for (char c = 'A'; c <= Letra; c++)
+                    {
+                        int preenchimento = (Tamanho - (c - 'A') * 2 - 1) / 2;
+                        Console.Write(new string(' ', preenchimento));
+                        for (char d = 'A'; d <= c; d++)
+                        {
+                            Console.Write(d);
+                            if (d != c)
+                            {
+                                Console.Write(' ');
+                            }
+                        }
+                        Console.Write(new string(' ', preenchimento));
+                        Console.WriteLine();
+                    }
+
+                    for (char c = (char)(Letra - 1); c >= 'A'; c--)
+                    {
+                        int preenchimento = (Tamanho - (c - 'A') * 2 - 1) / 2;
+                        Console.Write(new string(' ', preenchimento));
+                        for (char d = 'A'; d <= c; d++)
+                        {
+                            Console.Write(d);
+                            if (d != c)
+                            {
+                                Console.Write(' ');
+                            }
+                        }
+                        Console.Write(new string(' ', preenchimento));
+                        Console.WriteLine();
+                    }  
+        #endregion
+                }
+                else if (Escolha == 2)
+                {
+                    #region Diamante X
+                    Console.Clear();
+
+                    Console.Write("Digite um número ímpar: ");
+                    int tamanho = Convert.ToInt32(Console.ReadLine());
+
+                    int espacos = (tamanho - 1) / 2;
+
+                    int letras = 1;
+
+                    for (int i = 0; i < tamanho / 2; i++)
+                    {
+                        for (int j = 0; j < espacos; j++)
+                        {
+                            Console.Write(" ");
+                        }
+
+                        for (int j = 0; j < letras; j++)
+                        {
+                            Console.Write("x");
+                        }
+
+                        Console.WriteLine();
+
+                        espacos--;
+                        letras += 2;
+                    }
+
+                    for (int i = tamanho / 2; i >= 0; i--)
+                    {
+                        for (int j = 0; j < espacos; j++)
+                        {
+                            Console.Write(" ");
+                        }
+
+                        for (int j = 0; j < letras; j++)
+                        {
+                            Console.Write("x");
+                        }
+
+                        Console.WriteLine();
+
+                        espacos++;
+                        letras -= 2;
+                    }
+                    #endregion
+                }
+                else
+                {
+                    Console.WriteLine("Escolha uma opção válida");
+                    Console.WriteLine();
+
+                    goto inicio;
+                }
+
+
+            sair:
+                Console.WriteLine();
+
+                Console.WriteLine("Gostaria de fazer outro desenho? (S/N)");
+                ConsoleKeyInfo Opc = Console.ReadKey();
+
+                if (Opc.Key == ConsoleKey.S)
+                {
+                    Console.Clear();
+                    goto inicio;
+                }
+                else if (Opc.Key == ConsoleKey.N)
+                {
+                    Environment.Exit(0);
+                }
+                else
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("Opção inválida,tente novamente...");
+                    goto sair;
+                }
+			}
+			catch (Exception)
+			{
+                Console.Write("Erro ao desenhar, pressione qualquer botão para continuar...");
+                Console.ReadKey();
+
+                Console.Clear();
+                goto inicio;
+			}
+        }
+    }
+}
---
+++ Robô Tupiniquim/Robô Tupiniquim/Program.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Robô_Tupiniquim
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Robô Tupiniquim";
+
+            inicio:
+            try
+            {
+                Console.WriteLine("Informe a área de atuação do robo: (Ex: 2 0)");
+                string[] input = Console.ReadLine().Split();
+                int maxX = int.Parse(input[0]);
+                int maxY = int.Parse(input[1]);
+
+
+                while (true)
+                {
+                    Console.WriteLine();
+
+                    Console.WriteLine("Informe os eixos X, Y e Direção: (Ex: 0 0 L) ");
+                    input = Console.ReadLine().Split(); //0 0 L
+
+                    //Conversão dos dados acima
+                    int x = int.Parse(input[0]);
+                    int y = int.Parse(input[1]);
+                    char Direcao = char.Parse(input[2]);
+
+                    Console.WriteLine();
+
+                    Console.WriteLine("Insira as instruções: ");
+                    string Instrucoes = Console.ReadLine();
+
+                    foreach (char c in Instrucoes)
+                    {
+                        if (c == 'M')
+                        {
+                            switch (Direcao)
+                            {
+                                case 'N':
+                                    y++;
+                                    break;
+
+                                case 'S':
+                                    y--;
+                                    break;
+
+                                case 'L':
+                                    x++;
+                                    break;
+
+                                case 'O':
+                                    x--;
+                                    break;
+
+                            }
+                        }
+                        else
+                        {
+                            if (c == 'E') 
+                            {
+                                switch (Direcao)
+                                {
+                                    case 'N':
+                                        Direcao = 'O';
+                                        break;
+                                    case 'S':
+                                        Direcao = 'L';
+                                        break;
+                                    case 'L':
+                                        Direcao = 'N';
+                                        break;
+                                    case 'O':
+                                        Direcao = 'S';
+                                        break;
+                                }
+                            }
+                            else if (c == 'D')
+                            {
+                                switch (Direcao)
+                                {
+                                    case 'N':
+                                        Direcao = 'L';
+                                        break;
+                                    case 'S':
+                                        Direcao = 'O';
+                                        break;
+                                    case 'L':
+                                        Direcao = 'S';
+                                        break;
+                                    case 'O':
+                                        Direcao = 'N';
+                                        break;
+                                }
+                            }
+                        }
+                    }
+
+                    Console.WriteLine();
+
+                    Console.WriteLine("Posiçã atual: ");
+                    Console.WriteLine("X: " + x + " Y: " + y + " " + Direcao);
+
+                    Console.WriteLine();
+                }
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Erro, pressione qualquer botão para reiniciar...");
+                Console.ReadKey();
+
+                Console.Clear();
+                goto inicio;
+            }
+        }
+    }
+}
